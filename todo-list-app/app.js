0f9c34d1e831504776e787b7c83bd7c8fb2f10b5
kairavipandya <104184341+kairavipandya@users.noreply.github.com>
2024-01-08 13:06:46 -0600

    added new features

--- todo-list-app/app.js
+++ todo-list-app/app.js
@@ -32,16 +32,70 @@ app.get('/', (req, res) => {
 });
 
 app.post('/add', (req, res) => {
-  // Insert a new task into the database
-  const task = req.body.task;
-  db.query('INSERT INTO tasks (description) VALUES (?)', [task], (err, result) => {
-    if (err) throw err;
-    res.redirect('/');
+    const { task, category, due_date } = req.body;
+    const sql = 'INSERT INTO tasks (description, category, due_date) VALUES (?, ?, ?)';
+    db.query(sql, [task, category, due_date], (err, result) => {
+      if (err) throw err;
+      res.redirect('/');
+    });
   });
-});
+  
 
 // Start the server
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
+
+app.post('/mark-complete', (req, res) => {
+    const completedTasks = req.body.completedTasks;
+    if (completedTasks) {
+      db.query('UPDATE tasks SET completed = 1 WHERE id IN (?)', [completedTasks], (err, result) => {
+        if (err) throw err;
+        res.redirect('/');
+      });
+    } else {
+      res.redirect('/');
+    }
+  });
+
+
+  app.post('/delete', (req, res) => {
+    const deletedTasks = req.body.deletedTasks;
+    if (deletedTasks) {
+      db.query('DELETE FROM tasks WHERE id IN (?)', [deletedTasks], (err, result) => {
+        if (err) throw err;
+        res.redirect('/');
+      });
+    } else {
+      res.redirect('/');
+    }
+  });
+  
+
+  app.get('/edit/:id', (req, res) => {
+    const taskId = req.params.id;
+    db.query('SELECT * FROM tasks WHERE id = ?', [taskId], (err, results) => {
+      if (err) throw err;
+      if (results.length === 1) {
+        const task = results[0];
+        res.render('edit', { task: task });
+      } else {
+        res.redirect('/');
+      }
+    });
+  });
+  
+  app.post('/update/:id', (req, res) => {
+    const taskId = req.params.id;
+    const { description, category, due_date } = req.body;
+    db.query(
+      'UPDATE tasks SET description = ?, category = ?, due_date = ? WHERE id = ?',
+      [description, category, due_date, taskId],
+      (err, result) => {
+        if (err) throw err;
+        res.redirect('/');
+      }
+    );
+  });
+  
